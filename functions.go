6b8561f1bb398ac470a5641bfa9deb6cd6273404
Rex Lee <duguying2008@gmail.com>
2014-11-23 01:56:48 +0800

    json解码,创建文件夹

--- functions.go
+++ functions.go
@@ -99,6 +99,15 @@ func JsonEncode(data interface{}) (string, error) {
 	return string(a), err
 }
 
+// json解码
+func JsonDecode(data string) (interface{}, error) {
+	dataByte := []byte(data)
+	var dat interface{}
+
+	err := json.Unmarshal(dataByte, &dat)
+	return dat, err
+}
+
 // 检查文件或目录是否存在
 // 如果由 filename 指定的文件或目录存在则返回 true，否则返回 false
 func FileExist(filename string) bool {
@@ -106,7 +115,13 @@ func FileExist(filename string) bool {
 	return err == nil || os.IsExist(err)
 }
 
+// 字符串写入文件
 func WriteFile(fullpath string, str string) error {
 	data := []byte(str)
 	return ioutil.WriteFile(fullpath, data, 0644)
+}
+
+// 创建文件夹
+func Mkdir(path string) error {
+	return os.Mkdir(path, os.ModePerm)
 }
