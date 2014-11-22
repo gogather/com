268108a591ff9a5509e697ef7c908b3af424dba6
Rex Lee <duguying2008@gmail.com>
2014-11-23 00:27:12 +0800

    常用函数集

---
+++ functions.go
@@ -0,0 +1,112 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+// 随机串
+func RandString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+// 检查用户名
+func CheckUsername(username string) bool {
+	if username[0] >= '0' && username[0] <= '9' {
+		return false
+	}
+
+	for i := 0; i < len(username); i++ {
+		if !(username[i] == '_' ||
+			(username[i] >= '0' && username[i] <= '9') ||
+			(username[i] >= 'a' && username[i] <= 'z') ||
+			(username[i] >= 'A' && username[i] <= 'Z')) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Md5(value string) string {
+	h := md5.New()
+	h.Write([]byte(value))
+	return fmt.Sprintf("%s", hex.EncodeToString(h.Sum(nil)))
+}
+
+// 获取用户头像
+func GetGravatar(email string) string {
+	return "http://www.gravatar.com/avatar/" + Md5(strings.ToUpper(email))
+}
+
+// 检查文件或目录是否存在
+// 如果由 filename 指定的文件或目录存在则返回 true，否则返回 false
+func PathExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+// 读取文件
+func ReadFileByte(path string) ([]byte, error) {
+	fi, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	return ioutil.ReadAll(fi)
+}
+
+// 读取文本文件
+func ReadFile(path string) string {
+	fi, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	fd, err := ioutil.ReadAll(fi)
+
+	return string(fd)
+}
+
+// 切割关键词为html片段
+func TagSplit(keywords string) string {
+	if "" == keywords {
+		return ""
+	}
+
+	content := ""
+	tags := strings.Split(keywords, ",")
+	for _, value := range tags {
+		content = content + fmt.Sprintf(`<a class="tags" href="/tag/%s/1">%s</a>,`, value, value)
+	}
+	return content
+}
+
+// json编码
+func JsonEncode(data interface{}) (string, error) {
+	a, err := json.Marshal(data)
+	return string(a), err
+}
+
+// 检查文件或目录是否存在
+// 如果由 filename 指定的文件或目录存在则返回 true，否则返回 false
+func FileExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
+
+func WriteFile(fullpath string, str string) error {
+	data := []byte(str)
+	return ioutil.WriteFile(fullpath, data, 0644)
+}
