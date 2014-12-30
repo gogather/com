844678b2502d9314609bf48e54f0487da5596f98
Rex Lee <duguying2008@gmail.com>
2014-12-31 00:10:33 +0800

    copy function

--- functions.go
+++ functions.go
@@ -205,3 +205,18 @@ func Strim(str string) string {
 	str = strings.Replace(str, "\r", "", -1)
 	return str
 }
+
+// Copyfile
+func CopyFile(dstName, srcName string) (written int64, err error) {
+	src, err := os.Open(srcName)
+	if err != nil {
+		return
+	}
+	defer src.Close()
+	dst, err := os.OpenFile(dstName, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer dst.Close()
+	return io.Copy(dst, src)
+}
